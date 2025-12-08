3bc95f0b1caf4bc40177d01cbd9e905025e5361e
kevin russel <kzone159@gmail.com>
2025-12-07 18:45:07 -0600

    more changes to the drone class

--- dji.py
+++ dji.py
@@ -1,7 +1,16 @@
 class DJI:
+    
+    
+    
+    
+    
     def __init__(self):
         self.counter = 0
 
     def increase_counter(self):
         self.counter +=1
+
+        if (self.counter % 50 == 0):
+            print("lift off")
+            self.counter = 0
         return self.counter
