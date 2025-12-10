f05d87a80e708d37c1de887e813d7125b0ec9fd5
kevin russel <kzone159@gmail.com>
2025-12-09 21:12:43 -0600

    ahahah

--- dji.py
+++ dji.py
@@ -1,16 +1,26 @@
+from djitellopy import Tello
+
+import time
+import asyncio
+
 class DJI:
-    
-    
-    
-    
     
     def __init__(self):
         self.counter = 0
+        self.tello = Tello()
+        self.tello.connect()
 
-    def increase_counter(self):
+
+    async def liftoff(self):
+        self.tello.takeoff()
+        await asyncio.sleep(5)
+        self.tello.land()
+
+    def increase_counter(self): 
+        
         self.counter +=1
 
-        if (self.counter % 50 == 0):
-            print("lift off")
+        if (self.counter % 25 == 0):
+            self.liftoff()
             self.counter = 0
         return self.counter
