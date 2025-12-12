ca5989799c10f97d45aef5445b9fde766b709556
kevin russel <kzone159@gmail.com>
2025-12-11 22:21:50 -0600

    adding more flight controls

--- dji.py
+++ dji.py
@@ -9,6 +9,7 @@ class DJI:
         self.counter = 0
         self.right_counter = 0
         self.left_counter = 0
+        self.land_counter=0
         self.tello = Tello()
         self.tello.connect()
 
@@ -19,31 +20,37 @@ class DJI:
             if item == "liftoff":
                 self.liftoff()  
             elif item == "right":
-                self.move_right(20)
+                self.tello.move_right(20)
             elif item == "left":
-                self.move_left(20)
+                self.tello.move_left(20)
             elif item == "land":
-                self.land()
+                self.tello.land()
 
 
     def land_drone(self):
-        global_queue.put("land")
+        self.land_counter +=1
+        if(self.land_counter %100 ==0):
+            print("landing")
+            global_queue.put("land")
+            self.land_counter = 0
 
     def move(self, direction):
         if direction == "right":
             self.right_counter += 1
-            if(self.right_counter % 10 == 0):
+            if(self.right_counter % 100 == 0):
+                print("right")
                 global_queue.put(direction)
                 self.right_counter = 0
         else:
             self.left_counter += 1
-            if(self.left_counter % 10 == 0):
+            if(self.left_counter % 100 == 0):
+                print("left")
                 global_queue.put(direction)
                 self.left_counter = 0
 
     def liftoff(self):
         self.tello.takeoff()
-        self.tello.land()
+        self.tello.move_up(20)
 
     def takeoff_initiation(self): 
         self.counter +=1
